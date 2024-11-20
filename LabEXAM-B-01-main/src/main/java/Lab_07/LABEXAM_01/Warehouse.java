eb7df2da14582c2459dac8e14c0a21c9c475cdf5
Ridika-115 <168357177+ridika-2004@users.noreply.github.com>
2024-11-20 14:48:58 +0600

    Add files via upload

---
+++ LabEXAM-B-01-main/src/main/java/Lab_07/LABEXAM_01/Warehouse.java
@@ -0,0 +1,14 @@
+package Lab_07.LABEXAM_01;
+
+public class Warehouse {
+	public int id;
+	public String name;
+	public String location;
+
+	public Warehouse(int i, String name, String location) {
+		id=i;
+		this.name=name;
+		this.location=location;
+	}
+
+}
---
+++ LabEXAM-B-01-main/src/main/java/Lab_07/LABEXAM_01/reportScheduler.java
@@ -0,0 +1,7 @@
+package Lab_07.LABEXAM_01;
+
+class reportScheduler {
+    public static void scheduleReport(String reportType, String frequency) {
+        System.out.println("Scheduling " + reportType + " report with frequency " + frequency);
+    }
+}
